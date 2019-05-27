c9f4f8626bd7f85d54a737cc1b89e07f7ead7894
Overcash <dovercash@LT-67058.local>
2019-05-27 10:33:31 -0400

    Add part 36

--- section-six/part-thirtysix/app.js
+++ section-six/part-thirtysix/app.js
@@ -1,33 +1,50 @@
 const request = require('request');
-const key = '';
-const url = `https://api.darksky.net/forecast/${key}/37.8267,-122.4233`;
+// const key = '';
+// const url = `https://api.darksky.net/forecast/${key}/37.8267,-122.4233`;
+//
+// request({ url: url, json: true }, (error, res) => {
+// 	if (error) {
+// 		console.log('Unable to connect to weather service.');
+// 	} else if (res.body.error) {
+// 		console.log('Unable to find location.')
+// 	} else {
+// 		const temp = res.body.currently.temperature || 0;
+// 		const precip = res.body.currently.precipProbability || 0;
+// 		const dailySummary = res.body.daily.data[0].summary || '';
+// 		console.log(`${dailySummary} It is currently ${temp} degrees out. There is a ${(precip * 100).toFixed(2)}% chance of rain.`)
+// 	}
+// })
 
-const geoKey = '';
-const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
+const geocode = (location, callback) => {
+	const geoKey = '';
+	const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
 
-request({ url: url, json: true }, (error, res) => {
-	if (error) {
-		console.log('Unable to connect to weather service.');
-	} else if (res.body.error) {
-		console.log('Unable to find location.')
-	} else {
-		const temp = res.body.currently.temperature || 0;
-		const precip = res.body.currently.precipProbability || 0;
-		const dailySummary = res.body.daily.data[0].summary || '';
-		console.log(`${dailySummary} It is currently ${temp} degrees out. There is a ${(precip * 100).toFixed(2)}% chance of rain.`)
-	}
+	request( {url: `${geoUrl}${encodeURIComponent(location)}.json?access_token=${geoKey}&limit=1`, json: true }, (error, res) => {
+		if (error) {
+			callback('Unable to connect to geocoding service.');
+		} else if (res.body.message === 'Not Found' || !res.body.features.length) {
+			callback('Unable to find location.');
+		} else {
+			let coordinates = res.body.features[0].center || [];
+			const data = {
+				lat: coordinates[1],
+				long: coordinates[0],
+				location: res.body.features[0].place_name
+			}
+			callback(undefined, data);
+		}
+	})
+
+}
+
+// Common to show error and data for callbacks
+geocode('New York', (error, data) => {
+	console.log('ERROR: ', error);
+	console.log('DATA: ', data)
 })
 
-request({ url: `${geoUrl}Los%20Angeles.json?access_token=${geoKey}&limit=1`, json: true }, (error, res) => {
-	if (error) {
-		console.log('Unable to connect to geocoding service.')
-	} else if (res.body.message === 'Not Found' || !res.body.features.length) {
-		console.log('Unable to find location.')
-	} else {
-		let coordinates = res.body.features[0].center || [];
-		let lat = coordinates[1];
-		let long = coordinates[0];
-		console.log(`LAT: ${lat}`);
-		console.log(`LONG: ${long}`);
-	}
+// Common to show error and data for callbacks
+geocode('Peoria', (error, data) => {
+	console.log('ERROR: ', error);
+	console.log('DATA: ', data)
 })
